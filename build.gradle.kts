853cc5bfdd1f118a517419d91c3d4a687b7d4a89
Ji-Hwan Kim <ji-hwankim@Ji-Hwanui-Mac-Studio.local>
2023-07-31 20:34:13 +0900

    [Script push]

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.0"
+    kotlin("plugin.jpa") version "1.9.0"
+    kotlin("plugin.allopen") version "1.9.0"
+    kotlin("plugin.noarg") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
+}
+
+group = "studio.hcmc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":data-domain"))
+    implementation(project(":data-transfer-object"))
+    implementation(project(":data-value-object"))
+    implementation(project(":jpa-converter-extension"))
+    implementation(project(":kotlin-protocol-extension"))
+    implementation(project(":spring-controller"))
+    implementation(project(":spring-controller-extension"))
+    implementation(project(":spring-entity"))
+    implementation(project(":spring-repository"))
+    implementation(project(":spring-repository-extension"))
+    implementation(project(":spring-service"))
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime-jvm:0.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.5.1")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("com.mysql:mysql-connector-j")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/studio/hcmc/board/Application.kt
@@ -0,0 +1,21 @@
+package studio.hcmc.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+@ComponentScan(basePackages = [
+    "studio.hcmc.board.controller",
+    "studio.hcmc.board.repository",
+    "studio.hcmc.board.service",
+])
+class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+//            println(ObjectMapper.findModules().map { it.moduleName to it.dependencies })
+            runApplication<Application>()
+        }
+    }
+}
